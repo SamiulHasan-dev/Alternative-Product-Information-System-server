bad17e54f3265d509b5f149557af0cbbbe614d76
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-14 18:05:14 +0600

    delete remonnendation

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
             res.send(result)
         })
 
-        
+
         //delete my queries
         app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
@@ -66,6 +66,20 @@ async function run() {
             res.send(result);
         })
 
+        // // // update after recommandation
+        // app.put('/products/:id', async (req, res) => {
+        //     // Update recommendation count in product collection
+        //     const productId = req.params.id;
+        //     const query = { _id: new ObjectId(productId) };
+        //     const update = { $inc: { recommendationCount: 1 } }; // Increment recommendationCount by 1
+        //     const updateResult = await productCollection.updateOne(query, update);
+        //     res.send(updateResult)
+        // })
+
+
+
+
+
         //update Query
         app.put('/products/:id', async (req, res) => {
             const id = req.params.id;
@@ -73,17 +87,17 @@ async function run() {
             const options = { upsert: true }
             const updateQuery = req.body;
             const Query = {
-              $set: {
-                productName:updateQuery.productName,
-                productBand:updateQuery.productBand,
-                productImage:updateQuery.productImage,
-                queryTitle:updateQuery.queryTitle,
-                boycott:updateQuery.boycott,
-              }
+                $set: {
+                    productName: updateQuery.productName,
+                    productBand: updateQuery.productBand,
+                    productImage: updateQuery.productImage,
+                    queryTitle: updateQuery.queryTitle,
+                    boycott: updateQuery.boycott
+                }
             }
             const result = await productCollection.updateOne(filter, Query, options);
             res.send(result);
-          })
+        })
 
 
 
@@ -114,8 +128,128 @@ async function run() {
 
         /* Recommendation */
 
-        
 
+
+        //post tp recommend
+        // app.post('/recommendProduct', async (req, res) => {
+        //     const newProduct = req.body;
+        //     console.log(newProduct);
+        //     const result = await recommendationCollection.insertOne(newProduct);
+        //     res.send(result);
+        // })
+
+
+        //post and increase recommendationCount
+        app.post('/recommendProduct', async (req, res) => {
+            try {
+                const newProduct = req.body;
+                console.log(newProduct);
+
+                // Insert new recommendation into recommendationCollection
+                const result = await recommendationCollection.insertOne(newProduct);
+
+                // Extract productId from newProduct
+                const productId = newProduct.productId;
+                if (!productId) {
+                    throw new Error('productId is missing in the newProduct');
+                }
+
+                // Update recommendationCount in productCollection
+                const updateDoc = {
+                    $inc: { recommendationCount: 1 },
+                }
+                const jobQuery = { _id: new ObjectId(productId) }
+                const updateBidCount = await productCollection.updateOne(jobQuery, updateDoc)
+                console.log(updateBidCount)
+
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Internal Server Error');
+            }
+        });
+
+
+
+
+        //recommend by myEmail
+        app.get('/recommendProduct/:email', async (req, res) => {
+            console.log(req.params.email)
+            const myEmail = req.params.email;
+            const query = { recommenderEmail: myEmail };
+            console.log(myEmail)
+            const result = await recommendationCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        // app.get('/quizAttempt', async(req, res) => {
+        //     console.log(req.query.email);
+        //     // console.log('cook cookies', req.cookies);
+        //     // console.log('token owner info', req.user);
+        //     // if(req.query.email !== req.user.email){
+        //     //   return res.status(403).send({message: 'forbidden access'})
+        //     // }
+        //     let query = {};
+        //     if(req.query?.email){
+        //       query = {email: req.query.email}
+        //     }
+        //     const result = await taskCollection.find(query).toArray();
+        //     res.send(result);
+        //   })
+
+
+        app.get('/recommendProduct', async (req, res) => {
+            // const id = req.body();
+            const cursor = recommendationCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/recommendProductId/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await recommendationCollection.findOne(query);
+            res.send(result)
+        })
+        // delete the data
+        // app.delete('/recommendProductId/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await recommendationCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+
+        // Delete recommendation and decrement recommendationCount
+        app.delete('/recommendProductId/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) };
+
+                // Find the recommendation to get the productId
+                const recommendation = await recommendationCollection.findOne(query);
+                if (!recommendation) {
+                    res.status(404).send('Recommendation not found');
+                    return;
+                }
+
+                // Delete the recommendation
+                const result = await recommendationCollection.deleteOne(query);
+
+                // Decrement recommendationCount in productCollection
+                const productId = recommendation.productId;
+                const productQuery = { _id: new ObjectId(productId) };
+                const updateDoc = {
+                    $inc: { recommendationCount: -1 },
+                };
+                const updateResult = await productCollection.updateOne(productQuery, updateDoc);
+                console.log(updateResult);
+
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Internal Server Error');
+            }
+        });
 
 
 
